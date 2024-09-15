ae6c4aec13d5f647b0cdb9519241325c08953122
KhangVinh <vinhkhang378@gmail.com>
2024-09-15 12:43:36 +0700

    first commit

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ agent = Agent(env=env)
 app = Flask(__name__)
 CORS(app)
 
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend='redis://127.0.0.1:6379/0',
-        broker='redis://127.0.0.1:6379/0'
-    )
-    celery.conf.update(app.config)
-    return celery
+# def make_celery(app):
+#     celery = Celery(
+#         app.import_name,
+#         backend='redis://127.0.0.1:6379/0',
+#         broker='redis://127.0.0.1:6379/0'
+#     )
+#     celery.conf.update(app.config)
+#     return celery
 
-celery = make_celery(app)
+# celery = make_celery(app)
 
 '''Online Memory'''
 memory = defaultdict(list)
@@ -72,66 +72,78 @@ def store_transition():
         '''Store transition in memory'''
         memory[user_id].append(transition)
 
-    try:
-        res = requests.post("http://127.0.0.1:8019/train", json={"user_id": user_id})
-        if res.status_code != 200:
-            return jsonify({"status": "error", "message": "Failed to start training"}), 500
-        return jsonify({"status": "success", "message": "Transition stored and training started"}), 200
+    # try:
+    #     res = requests.post("http://127.0.0.1:8019/train", json={"user_id": user_id})
+    #     if res.status_code != 200:
+    #         return jsonify({"status": "error", "message": "Failed to start training"}), 500
+    #     return jsonify({"status": "success", "message": "Transition stored and training started"}), 200
 
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+    # except Exception as e:
+    #     return jsonify({"status": "error", "message": str(e)}), 500
 
     return jsonify({"status": "success"}), 200
 
 # Train API that uses Celery to train in the background
-@app.route("/train", methods=["POST"])
-def train_model():
-    user_id = request.json["user_id"]
+# @app.route("/train", methods=["POST"])
+# def train_model():
+#     user_id = request.json["user_id"]
 
-    if user_id not in memory:
-        return jsonify({"message": "No transitions to train"}), 400
+#     if user_id not in memory:
+#         return jsonify({"message": "No transitions to train"}), 400
     
-    print(f"Transitions for user {user_id}:")
-    for transition in memory[user_id]:
-        print(transition)
+#     print(f"Transitions for user {user_id}:")
+#     for transition in memory[user_id]:
+#         print(transition)
 
-    # Pass the transitions to the background training task
-    task = background_train.apply_async(args=[user_id, memory[user_id]])
+#     # Pass the transitions to the background training task
+#     task = background_train.apply_async(args=[user_id, memory[user_id]])
     
-    return jsonify({"message": "Training started!", "task_id": task.id})
+#     return jsonify({"message": "Training started!", "task_id": task.id})
 
-'''Background training task using Celery'''
-@celery.task
-def background_train(user_id, transitions):
-    print(f"Training model for user {user_id}...")
+# '''Background training task using Celery'''
+# @celery.task
+# def background_train(user_id, transitions):
+#     print(f"Training model for user {user_id}...")
 
-    score = 0
-    load_checkpoint = False
+#     score = 0
+#     load_checkpoint = False
 
-    for transition in transitions:
-        state = transition["state"]
-        action = transition["action"]
-        next_state = transition["next_state"]
-        reward = transition["reward"]
-        done = transition["done"]
+#     for transition in transitions:
+#         state = transition["state"]
+#         action = transition["action"]
+#         next_state = transition["next_state"]
+#         reward = transition["reward"]
+#         done = transition["done"]
         
-        '''Set agent action'''
-        agent.action = action
+#         '''Set agent action'''
+#         agent.action = action
 
-        '''Learn'''
-        score += reward
-        if not load_checkpoint:
-            agent.learn(state, reward, next_state, done)
-        score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
+#         '''Learn'''
+#         score += reward
+#         if not load_checkpoint:
+#             agent.learn(state, reward, next_state, done)
+#         score_history.append(score)
+#         avg_score = np.mean(score_history[-100:])
 
-        if avg_score > best_score:
-            best_score = avg_score
-            if not load_checkpoint:
-                agent.save_models()
+#         if avg_score > best_score:
+#             best_score = avg_score
+#             if not load_checkpoint:
+#                 agent.save_models()
         
-    Print.success(f"Training completed for user {user_id}.")
-    return "Training finished"
+#     Print.success(f"Training completed for user {user_id}.")
+#     return "Training finished"
+
+# '''Get status of training task'''
+# @app.route("/status/<task_id>")
+# def get_status(task_id):
+#     task = AsyncResult(task_id, app=celery)
+#     if task.state == "PENDING":
+#         response = {"state": task.state, "status": "Pending..."}
+#     elif task.state != "FAILURE":
+#         response = {"state": task.state, "result": task.result}
+#     else:
+#         response = {"state": task.state, "result": str(task.info)}
+#     return jsonify(response)
 
 '''Get memory'''
 @app.route("/get_memory", methods=["GET"])
@@ -150,18 +162,6 @@ def get_memory_by_user_id(user_id):
         return jsonify({"status": "error", "message": "User ID not found"}), 404
     return jsonify(memory[user_id]), 200
 
-'''Get status of training task'''
-@app.route("/status/<task_id>")
-def get_status(task_id):
-    task = AsyncResult(task_id, app=celery)
-    if task.state == "PENDING":
-        response = {"state": task.state, "status": "Pending..."}
-    elif task.state != "FAILURE":
-        response = {"state": task.state, "result": task.result}
-    else:
-        response = {"state": task.state, "result": str(task.info)}
-    return jsonify(response)
-
 
 '''Predict action'''
 @app.route('/predict_action', methods=['POST'])
@@ -175,7 +175,6 @@ def predict_action():
         return jsonify({"action": action, "exercise": exercise}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
-
 
 '''Run Flask app'''
 if __name__ == '__main__':
